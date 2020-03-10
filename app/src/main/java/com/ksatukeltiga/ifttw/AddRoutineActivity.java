38656c0cf7ef194e3ba615d91ad31ac1be52b232
sy-anwar <13517139@std.stei.itb.ac.id>
2020-03-10 12:29:08 +0700

    Init Project, add Main Activity and Add Routine Activity

---
+++ app/src/main/java/com/ksatukeltiga/ifttw/AddRoutineActivity.java
@@ -0,0 +1,68 @@
+package com.ksatukeltiga.ifttw;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+public class AddRoutineActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_routine);
+
+        Spinner conditionSpinner = findViewById(R.id.conditionSpinner);
+        Spinner actionSpinner = findViewById(R.id.actionSpinner);
+
+        conditionSpinner.setOnItemSelectedListener(this);
+        actionSpinner.setOnItemSelectedListener(this);
+
+        ArrayAdapter<CharSequence> conditionAdapter = ArrayAdapter.createFromResource(this,
+                R.array.condition_module, R.layout.support_simple_spinner_dropdown_item);
+
+        ArrayAdapter<CharSequence> actionAdapter = ArrayAdapter.createFromResource(this,
+                R.array.action_module, R.layout.support_simple_spinner_dropdown_item);
+
+        conditionAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        actionAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        conditionSpinner.setAdapter(conditionAdapter);
+        actionSpinner.setAdapter(actionAdapter);
+
+        FloatingActionButton fab = findViewById(R.id.saveFab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // AddRoutineActivity.saveRoutine();
+                startActivity(new Intent(AddRoutineActivity.this, MainActivity.class));
+            }
+        });
+    }
+
+    private static void saveRoutine() {
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String item = parent.getItemAtPosition(position).toString();
+
+        // Showing selected spinner item
+        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
