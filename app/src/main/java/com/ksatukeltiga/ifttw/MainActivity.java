5bb9524c6930a0aaabfea983706fbf5fb78512a1
sy-anwar <13517139@std.stei.itb.ac.id>
2020-03-11 09:21:45 +0700

    add gradle script

--- app/src/main/java/com/ksatukeltiga/ifttw/MainActivity.java
+++ app/src/main/java/com/ksatukeltiga/ifttw/MainActivity.java
@@ -38,12 +38,10 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
-
             }
 
             @Override
             public void onTabReselected(TabLayout.Tab tab) {
-
             }
         });
 
